673823c8e176b5ce9dee1b7ac6a9bcd8a6994e7e
Victor Jørgensen <victor.jorgens@gmail.com>
2018-11-13 15:01:57 +0100

    .

--- j/Bbconcop.py
+++ j/Bbconcop.py
@@ -277,7 +277,7 @@ class Behavior:
 
 class Approach(Behavior): #UV-behaviour
     def __init__(self, bbcon):
-        super(Behavior).__init__()
+        super().__init__(bbcon)
         self.sensobs.append(LookAhead()) #UV
         self.active_flag=True
         self.bbcon.active_behav.append(self)
